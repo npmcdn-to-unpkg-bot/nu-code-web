e0f26b090af90ad82a5f4e25847ddcb3127e9bff
Tyler Berry <tyler@berryac.com>
2016-07-12 19:50:11 -0600

    install angularfire2 and firebase

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 import { AppComponent, appRouterProviders, environment } from './app/';
 
 if (environment.production) {
@@ -7,6 +8,13 @@ if (environment.production) {
 }
 
 bootstrap(AppComponent, [
-  appRouterProviders
+  appRouterProviders,
+  FIREBASE_PROVIDERS,
+  defaultFirebase({
+    apiKey: "AIzaSyBJeW4SZUNUySEHd7GLc1qwBxsNLgN2a8Y",
+    authDomain: "nu-code-350ea.firebaseapp.com",
+    databaseURL: "https://nu-code-350ea.firebaseio.com",
+    storageBucket: "nu-code-350ea.appspot.com",
+  })
 ])
 .catch(err => console.error(err));
