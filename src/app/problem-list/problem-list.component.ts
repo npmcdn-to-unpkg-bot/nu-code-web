4a0ee73b4ff7de797b62e12081c04cfc78c81eaa
Tyler Berry <tyler@berryac.com>
2016-07-12 20:02:09 -0600

    pulls data from firebase

--- src/app/problem-list/problem-list.component.ts
+++ src/app/problem-list/problem-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   moduleId: module.id,
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['problem-list.component.css']
 })
 export class ProblemListComponent implements OnInit {
+  problems: FirebaseListObservable<any[]>;
 
-  constructor() {}
+  constructor(af: AngularFire) {
+    this.problems = af.database.list('/problems');
+  }
 
   ngOnInit() {
   }
