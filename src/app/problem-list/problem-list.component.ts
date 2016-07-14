b48e9b81e2437f18b5b4c3b9317d78c1de6a51c0
Tyler Berry <tyler@berryac.com>
2016-07-13 20:49:31 -0600

    using problem-preview component to in problem-list

--- src/app/problem-list/problem-list.component.ts
+++ src/app/problem-list/problem-list.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Problem } from '../shared/problem';
+import { ProblemPreviewComponent } from '../problem-preview';
 
 @Component({
   moduleId: module.id,
   selector: 'app-problem-list',
   templateUrl: 'problem-list.component.html',
-  styleUrls: ['problem-list.component.css']
+  styleUrls: ['problem-list.component.css'],
+  directives: [ProblemPreviewComponent]
 })
 export class ProblemListComponent implements OnInit {
-  problems: FirebaseListObservable<any[]>;
+  problems: FirebaseListObservable<Problem[]>;
 
   constructor(af: AngularFire) {
     this.problems = af.database.list('/problems');
