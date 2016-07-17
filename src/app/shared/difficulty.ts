f7cde82e64648bf498489016b469198f2cc49257
Tyler Berry <tyler@berryac.com>
2016-07-17 17:52:25 -0600

    simpler difficulty enum

--- src/app/shared/difficulty.ts
+++ src/app/shared/difficulty.ts
@@ -4,38 +4,3 @@ export enum Difficulty {
   Medium,
   Hard
 }
-export namespace Difficulty {
-  export function fromString(difficulty: string): Difficulty {
-    let value;
-    switch (difficulty.toLowerCase()) {
-      case 'beginner':
-        value = Difficulty.Beginner;
-      case 'easy':
-        value = Difficulty.Easy;
-      case 'medium':
-        value = Difficulty.Medium;
-      case 'hard':
-        value = Difficulty.Hard;
-      default:
-        value = null;
-    }
-    return value;
-  }
-
-  // export function toString(difficulty: Difficulty): string {
-  //   let value;
-  //   switch (difficulty) {
-  //     case difficulty.:
-  //       value = Difficulty.Beginner;
-  //     case 'easy':
-  //       value = Difficulty.Easy;
-  //     case 'medium':
-  //       value = Difficulty.Medium;
-  //     case 'hard':
-  //       value = Difficulty.Hard;
-  //     default:
-  //       value = null;
-  //   }
-  //   return value;
-  // }
-}
