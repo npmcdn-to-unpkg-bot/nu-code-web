76a8d066862adc99d1fdcf0f86455b41803d3a72
Tyler Berry <tyler@berryac.com>
2016-07-16 23:53:56 -0600

    expanded on problem.component

--- src/app/problem/problem.component.ts
+++ src/app/problem/problem.component.ts
@@ -10,7 +10,13 @@ import { Problem, ProblemService } from '../shared';
   styleUrls: ['problem.component.css']
 })
 export class ProblemComponent implements OnInit, OnDestroy {
+  // TODO: can this be const?
+  CharacterLimit = 100000;
+
   problem: Problem;
+  currentTab: Tab;
+
+  sourceCode: string;
 
   // A connection opened in ngOnInit(), closed in ngOnDestroy()
   private subscription: Subscription;
@@ -21,14 +27,17 @@ export class ProblemComponent implements OnInit, OnDestroy {
     private problemService: ProblemService) {}
 
   ngOnInit() {
+    this.currentTab = Tab.Problem;
     this.subscription = this.route
       .params
       .subscribe(params => {
         let id = +params['id'];
+        // TODO: grab which tab from route data
         this.problemService.getProblem(id)
           .subscribe(problem => {
             // TODO: could use some more elegant validation that the problem exists
             if (problem.name) {
+              console.log('found problem');
               this.problem = problem;
             } else {
               this.goToProblemsList();
@@ -43,8 +52,28 @@ export class ProblemComponent implements OnInit, OnDestroy {
     }
   }
 
+  isCurrentTab(tab: string): boolean {
+    return this.currentTab == Tab[tab];
+  }
+
+  goToProblemTab(): void {
+    this.currentTab = Tab.Problem;
+  }
+
+  goToMySubmissionsTab(): void {
+    this.currentTab = Tab.MySubmissions;
+  }
+
+  goToLeaderboardTab(): void {
+    this.currentTab = Tab.Leaderboard;
+  }
+
   goToProblemsList() {
     this.router.navigate(['/problems']);
+  }
+
+  numChars() {
+    return this.sourceCode ? this.sourceCode.length : 0;
   }
 
   // // TODO: incorporate the passing along of id into `goToProblemsList`
@@ -56,4 +85,10 @@ export class ProblemComponent implements OnInit, OnDestroy {
   //   // Absolute link
   //   this.router.navigate(['/crisis-center', {id: crisisId, foo: 'foo'}]);
   // }
+}
+
+enum Tab {
+  Problem,
+  MySubmissions,
+  Leaderboard
 }
