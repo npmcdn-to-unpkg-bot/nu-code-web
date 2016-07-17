177b2d59575550d57baf208c8c42a6b9f4f1e451
Tyler Berry <tyler@berryac.com>
2016-07-16 18:45:20 -0600

    added a TODO with splitting routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ProblemComponent } from './problem';
 import { ProblemListComponent } from './problem-list';
 import { CompetitionListComponent } from './competition-list';
 
+// TODO: this can be split up. See: http://plnkr.co/edit/QVWco9Yfzp4vpf59Y4sQ?p=preview
 const routes: RouterConfig = [
   { path: '', component: HomeComponent },
   { path: 'problems', component: ProblemListComponent },
