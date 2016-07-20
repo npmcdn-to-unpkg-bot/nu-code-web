315287c94f5df6857823940c95c2cd8592188938
Tyler Berry <tyler@berryac.com>
2016-07-19 19:39:03 -0600

    removed test ng2-bootstrap alert from main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { ProblemService, UserService } from './shared';
-import { AlertComponent } from 'ng2-bootstrap';
 
 @Component({
   moduleId: module.id,
