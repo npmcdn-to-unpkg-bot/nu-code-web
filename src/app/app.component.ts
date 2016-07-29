731a848be6bccff2d2d3e0239403e99973bccd68
Tyler Berry <tyler@berryac.com>
2016-07-29 17:43:13 -0600

    extracted navbar in app.component to navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,25 @@
 import { Component, ViewContainerRef } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { AuthService, ProblemService, UserService } from './shared';
+import { NavbarComponent } from './navbar';
+import { ProblemService, UserService } from './shared';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ROUTER_DIRECTIVES],
+  directives: [
+    ROUTER_DIRECTIVES,
+    NavbarComponent
+  ],
   providers: [
     HTTP_PROVIDERS,
-    AuthService,
     ProblemService,
     UserService
   ]
 })
 export class AppComponent {
-  constructor(
-    // viewContainerRef is needed for angular2-bootstrap modals
-    private viewContainerRef: ViewContainerRef,
-    private authService: AuthService) { }
-
-  logIn(): void {
-    this.authService.logInWithEmailPassword('tberry@student.neumont.edu', 'samplepassword');
-  }
-
-  logOut(): void {
-    this.authService.logOut();
-  }
+  // viewContainerRef is needed for angular2-bootstrap modals
+  constructor(private viewContainerRef: ViewContainerRef) { }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared';
 
 @Component({
   moduleId: module.id,
   selector: 'app-navbar',
   templateUrl: 'navbar.component.html',
-  styleUrls: ['navbar.component.css']
+  styleUrls: ['navbar.component.css'],
+  providers: [AuthService]
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  navigate(path: string): Promise<boolean> {
+    return this.router.navigateByUrl(path);
+  }
+
+  logIn(): void {
+    this.authService.logInWithEmailPassword('tberry@student.neumont.edu', 'samplepassword');
+  }
+
+  logOut(): void {
+    this.authService.logOut();
   }
 
 }
