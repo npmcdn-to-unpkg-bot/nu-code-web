390952d4df80740d3f85a570019b1661e6892f0d
Tyler Berry <tyler@berryac.com>
2016-07-29 18:48:02 -0600

    request headers are const now

--- src/app/shared/submission.service.ts
+++ src/app/shared/submission.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
 import { Result, Submission } from './';
 
 const Url = 'http://172.17.0.2:8080/api';
-let headers = new Headers();
-headers.append('Content-Type', 'application/json');
+const RequestHeaders = new Headers({
+  'Content-Type': 'application/json'
+});
 
 @Injectable()
 export class SubmissionService {
@@ -13,9 +14,9 @@ export class SubmissionService {
 
   submit(submission: Submission): Observable<Result> {
     return this.http
-        .post(Url, submission, {headers: headers})
-        .map(res => {
-          return res.json() as Result;
-        });
+      .post(Url, submission, {headers: RequestHeaders})
+      .map(res => {
+        return res.json() as Result;
+      });
   }
 }
