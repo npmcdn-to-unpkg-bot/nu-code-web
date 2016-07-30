6993370329f5655705b3c05b147a8b0c420ef93e
Tyler Berry <tyler@berryac.com>
2016-07-30 04:44:40 -0600

    login-modal actually sends email/pass

--- src/app/login-modal/login-modal.component.ts
+++ src/app/login-modal/login-modal.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 import {
   BS_VIEW_PROVIDERS,
   MODAL_DIRECTIVES,
   ModalDirective
 } from 'ng2-bootstrap/ng2-bootstrap';
+import { FaDirective } from 'angular2-fontawesome/directives';
 import { AuthService } from '../shared';
 
 @Component({
@@ -12,11 +14,20 @@ import { AuthService } from '../shared';
   selector: 'app-login-modal',
   templateUrl: 'login-modal.component.html',
   styleUrls: ['login-modal.component.css'],
-  directives: [MODAL_DIRECTIVES],
+  directives: [
+    MODAL_DIRECTIVES,
+    FaDirective
+  ],
   viewProviders: [BS_VIEW_PROVIDERS]
 })
 export class LoginModalComponent implements OnInit {
   @ViewChild('modal') modal: ModalDirective;
+  @ViewChild('loginForm') loginForm: NgForm;
+
+  email: string;
+  password: string;
+
+  failed: boolean = false;
 
   constructor(
     private router: Router,
@@ -30,10 +41,28 @@ export class LoginModalComponent implements OnInit {
   }
 
   logInWithEmailPassword(): void {
-    // Show spinner
-    this.authService.logInWithEmailPassword('tberry@student.neumont.edu', 'samplepassword').then(
-        () => this.modal.hide(),
-        err => console.log(err));
+    if (this.loginForm.valid) {
+      this.failed = false;
+      // Show spinner
+      this.authService.logInWithEmailPassword(this.email, this.password).then(
+          () => {
+            this.modal.hide();
+          },
+          err => {
+            this.failed = true;
+            this.password = '';
+            console.log(err);
+          });
+    }
+  }
+
+  cancel(): void {
+    this.modal.hide();
+  }
+
+  onHide(): void {
+    this.email = '';
+    this.password = '';
   }
 
 }
