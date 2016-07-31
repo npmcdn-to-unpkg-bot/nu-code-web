845561c8b4e4603fadb150c9dc2b21aaf98a3f0a
Tyler Berry <tyler@berryac.com>
2016-07-31 17:29:03 -0600

    leaderboard tab shows default none

--- src/app/problem/leaderboard/leaderboard.component.ts
+++ src/app/problem/leaderboard/leaderboard.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'app-leaderboard',
   templateUrl: 'leaderboard.component.html',
-  styleUrls: ['leaderboard.component.css']
+  styleUrls: ['leaderboard.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class LeaderboardComponent implements OnInit {
 
