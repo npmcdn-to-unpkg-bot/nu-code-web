6bb5775ffd5b1bc854e8d273c2e148fbc0b4c45a
Tyler Berry <tyler@berryac.com>
2016-07-31 17:18:15 -0600

    my-submissions shows default no submissions

--- src/app/problem/my-submissions/my-submissions.component.ts
+++ src/app/problem/my-submissions/my-submissions.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'app-my-submissions',
   templateUrl: 'my-submissions.component.html',
-  styleUrls: ['my-submissions.component.css']
+  styleUrls: ['my-submissions.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class MySubmissionsComponent implements OnInit {
 
