1c4e3e9ceb45ccb36500635eb19b6c2f3cac2ad5
Tyler Berry <tyler@berryac.com>
2016-07-31 23:09:01 -0600

    small profile view

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User, RepositoryService } from '../shared';
 
 @Component({
   moduleId: module.id,
@@ -7,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+      private route: ActivatedRoute,
+      private repoService: RepositoryService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+        params => {
+          let id = params['id'];
+          this.repoService.getUser(id).subscribe(user => this.user = user);
+        });
   }
 
 }
