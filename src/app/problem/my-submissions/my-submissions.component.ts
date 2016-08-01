4f20eea8fe25d432f42db0df7413fc3459bfdd4f
Tyler Berry <tyler@berryac.com>
2016-07-31 22:51:28 -0600

    my-submissions shows rather jumpily, but shows

--- src/app/problem/my-submissions/my-submissions.component.ts
+++ src/app/problem/my-submissions/my-submissions.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { AngularFire } from 'angularfire2';
 import { FaDirective } from 'angular2-fontawesome/directives';
 import {
   AuthService,
@@ -31,28 +30,25 @@ export class MySubmissionsComponent implements OnInit {
   mySubmissions: MySubmission[];
 
   constructor(
-      private af: AngularFire,
       private authService: AuthService,
       private sharingService: SharingService,
       private repoService: RepositoryService) { }
 
   ngOnInit() {
-    // if (this.authService.auth) {
-    this.af.auth.subscribe(
-        auth => {
-          if (auth) {
-            if (this.sharingService.problem) {
-              this.initMySubmissions(this.sharingService.problem.$key);
-            } else {
-              this.sharingService.problemObservable.subscribe(
-                  problem => this.initMySubmissions(problem.$key));
-            }
+    // Do not try to load submissions until the user is logged in
+    this.authService.user.subscribe(
+        user => {
+          if (user) {
+            this.initMySubmissions(user.$key, this.sharingService.problem.$key);
+          } else {
+            this.mySubmissions = [];
           }
-        });
+        }
+    );
   }
 
-  initMySubmissions(problemId: string) {
-    this.repoService.getSubmissions(this.authService.auth.uid, problemId).subscribe(
+  initMySubmissions(userId: string, problemId: string) {
+    this.repoService.getSubmissions(userId, problemId).subscribe(
         submissions => this.mySubmissions = submissions);
   }
 }
