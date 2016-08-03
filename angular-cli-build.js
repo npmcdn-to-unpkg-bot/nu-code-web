b48344da3238390c4b9b10dca63ece0e782f033c
Tyler Berry <tyler@berryac.com>
2016-08-03 10:01:13 -0600

    jquery/bootstrap used locally

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -17,6 +17,10 @@ module.exports = function(defaults) {
       'reflect-metadata/**/*.+(ts|js|js.map)',
       'rxjs/**/*.+(js|js.map)',
       '@angular/**/*.+(js|js.map)',
+      // jQuery
+      'jquery/dist/jquery.min.js',
+      // Bootstrap
+      'bootstrap/dist/**',
       // angularfire2
       'angularfire2/**/*.js',
       'firebase/*.js',
