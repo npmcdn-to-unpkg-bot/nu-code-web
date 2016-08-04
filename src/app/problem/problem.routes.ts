87397cbb23d9ea752ba8771ce799241459b4ca92
Tyler Berry <tyler@berryac.com>
2016-08-03 20:45:21 -0600

    better looking subrouting for problem page

--- src/app/problem/problem.routes.ts
+++ src/app/problem/problem.routes.ts
@@ -9,8 +9,7 @@ export const ProblemRoutes: RouterConfig = [
     path: 'problems/:id',
     component: ProblemComponent,
     children: [
-      { path: '', redirectTo: 'solve' },
-      { path: 'solve', component: ViewComponent },
+      { path: '', component: ViewComponent },
       { path: 'my-submissions', component: MySubmissionsComponent },
       { path: 'leaderboard', component: LeaderboardComponent }
     ]
