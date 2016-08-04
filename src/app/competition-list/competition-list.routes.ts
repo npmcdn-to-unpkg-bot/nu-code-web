d6eaff86900fe232ac95b649fe226331bb18a950
Tyler Berry <tyler@berryac.com>
2016-08-03 19:28:54 -0600

    competition-list requires verification too

--- src/app/competition-list/competition-list.routes.ts
+++ src/app/competition-list/competition-list.routes.ts
@@ -1,11 +1,14 @@
 import { RouterConfig } from '@angular/router';
 import { CompetitionListComponent } from './';
-import { LoggedInGuard } from '../shared';
+import { LoggedInGuard, VerifiedGuard } from '../shared';
 
 export const CompetitionListRoutes: RouterConfig = [
   {
     path: 'competitions',
     component: CompetitionListComponent,
-    canActivate: [LoggedInGuard]
+    canActivate: [
+      LoggedInGuard,
+      VerifiedGuard
+    ]
   }
 ];
