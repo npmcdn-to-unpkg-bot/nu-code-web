767bbfc3439d6c9f8b54d7a5d3115048516d3466
Tyler Berry <tyler@berryac.com>
2016-08-06 18:23:05 -0600

    basic form for create-problem

--- src/app/create-problem/create-problem.component.ts
+++ src/app/create-problem/create-problem.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  FORM_DIRECTIVES,
+  REACTIVE_FORM_DIRECTIVES
+} from '@angular/forms';
+import { AuthService, Problem, RepositoryService, TestCase, } from '../shared';
 
 @Component({
   moduleId: module.id,
   selector: 'app-create-problem',
   templateUrl: 'create-problem.component.html',
-  styleUrls: ['create-problem.component.css']
+  styleUrls: ['create-problem.component.css'],
+  directives: [
+    FORM_DIRECTIVES,
+    REACTIVE_FORM_DIRECTIVES
+  ]
 })
 export class CreateProblemComponent implements OnInit {
+  problem = new Problem();
+  testCases = [new TestCase()];
+  @ViewChild('form') form;
 
-  constructor() { }
+  constructor(
+      private authService: AuthService,
+      private repoService: RepositoryService) { }
 
   ngOnInit() {
+    // Set creator to the logged in user's uid
+    this.authService.auth.take(1).subscribe(auth => this.problem.creatorUid = auth.uid);
   }
 
+  addTestCase(): void {
+    this.testCases.push(new TestCase());
+  }
+
+  submit(): void {
+    console.log(this.form.value);
+    // TODO: loop through test cases and set hints to undefined if ''
+    console.log(this.problem);
+    console.log(this.testCases);
+  }
 }
