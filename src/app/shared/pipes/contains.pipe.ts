b0f99a955bea1f0c4df0e4dd19ad2892e0a309bd
Tyler Berry <tyler@berryac.com>
2016-08-08 01:49:14 -0600

    better search

--- src/app/shared/pipes/contains.pipe.ts
+++ src/app/shared/pipes/contains.pipe.ts
@@ -8,13 +8,35 @@ export class ContainsPipe implements PipeTransform {
   transform(allProblems: Problem[], query: string): Problem[] {
     return allProblems
         ? allProblems.filter(problem => {
-            let nameMatches = containsIgnoreCase(problem.name, query);
+            let nameMatches = matchesIgnoreCase(problem.name, query);
             return nameMatches;
           })
         : null;
   }
 }
 
-function containsIgnoreCase(bigString: string, lilString: string): boolean {
-  return bigString.toLowerCase().includes(lilString.toLowerCase());
+function matchesIgnoreCase(big: string, lil: string) {
+  return fuzzySearch(big.toLowerCase(), lil.toLowerCase());
 }
+
+function fuzzySearch(big: string, lil: string) {
+  let tlen = big.length;
+  let qlen = lil.length;
+  if (qlen > tlen) {
+    return false;
+  }
+  if (qlen === tlen) {
+    return lil === big;
+  }
+  outer: for (let i = 0, j = 0; i < qlen; i++) {
+    let nch = lil.charCodeAt(i);
+    while (j < tlen) {
+      if (big.charCodeAt(j++) === nch) {
+        continue outer;
+      }
+    }
+    return false;
+  }
+  return true;
+}
+
