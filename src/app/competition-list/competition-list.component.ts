6ad4df3750eda2c8cb792591de8cbeff3be3d989
Tyler Berry <tyler@berryac.com>
2016-08-08 22:58:22 -0600

    competition-list shows create button

--- src/app/competition-list/competition-list.component.ts
+++ src/app/competition-list/competition-list.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { FaDirective } from 'angular2-fontawesome/directives';
+import { AuthService, RepositoryService } from '../shared';
 
 @Component({
   moduleId: module.id,
   selector: 'app-competition-list',
   templateUrl: 'competition-list.component.html',
-  styleUrls: ['competition-list.component.css']
+  styleUrls: ['competition-list.component.css'],
+  directives: [
+    ROUTER_DIRECTIVES,
+    FaDirective
+  ]
 })
 export class CompetitionListComponent implements OnInit {
+  canCreateCompetition: boolean;
 
-  constructor() {}
+  constructor(
+      private authService: AuthService,
+      private repoService: RepositoryService) {}
 
   ngOnInit() {
+    this.authService.isNeumonter
+        .subscribe(isNeumonter => this.canCreateCompetition = isNeumonter);
   }
-
 }
