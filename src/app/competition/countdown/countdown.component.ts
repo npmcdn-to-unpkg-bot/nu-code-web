17f5ca36c1d4617f214db8350e6c4290aaf11586
Tyler Berry <tyler@berryac.com>
2016-08-10 17:58:45 -0600

    very basic countdown redirection

--- src/app/competition/countdown/countdown.component.ts
+++ src/app/competition/countdown/countdown.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+import { RepositoryService } from '../../shared';
 
 @Component({
   moduleId: module.id,
@@ -7,10 +10,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['countdown.component.css']
 })
 export class CountdownComponent implements OnInit {
+  startTime: Date;
+  remainingTime: Time;
 
-  constructor() { }
+  constructor(
+      private router: Router,
+      private route: ActivatedRoute,
+      private repoService: RepositoryService) { }
 
   ngOnInit() {
-  }
+    let competitionId = this.route.snapshot.params['id'];
+    this.repoService.getCompetitionStartTime(competitionId).subscribe(
+        time => {
+          this.startTime = time;
+        });
 
+    // Start the timer
+    let subscription = Observable.interval(1000).subscribe(() => {
+      let now = new Date();
+      let remainingMilliseconds = this.startTime.getTime() - now.getTime();
+      let remainingSeconds = Math.floor(remainingMilliseconds / 1000);
+      if (remainingSeconds > 0) {
+        this.remainingTime = Time.fromSeconds(remainingSeconds);
+      } else {
+        this.router.navigate(['competitions', competitionId]);
+        subscription.unsubscribe();
+      }
+    });
+  }
+}
+
+interface Time {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+namespace Time {
+  export function fromSeconds(seconds: number): Time {
+    console.log(seconds);
+    let days = Math.floor(seconds / 86400);
+    seconds -= days * 86400;
+
+    let hours = Math.floor(seconds / 3600);
+    seconds -= hours * 3600;
+
+    let minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+
+    return { days, hours, minutes, seconds };
+  }
 }
