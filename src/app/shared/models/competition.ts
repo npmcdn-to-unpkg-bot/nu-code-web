d728645c9ff77acc5e5731c35b57252055af51bd
Tyler Berry <tyler@berryac.com>
2016-08-09 23:28:48 -0600

    removed the notion of a round, at least for now

--- src/app/shared/models/competition.ts
+++ src/app/shared/models/competition.ts
@@ -3,36 +3,24 @@ import { Feedback } from './feedback';
 export interface Competition {
   $key?: string;
   name: string;
-  rounds: Round[];
+  problems: CompetitionProblem[];
+  startTime: Date;
+  endTime: Date;
   creatorUid: string;
 }
 
 export class Competition implements Competition {
   constructor(
       name = '',
-      rounds = [new Round()],
-      creatorUid: string = null) {
-    this.name = name;
-    this.rounds = rounds;
-    this.creatorUid = creatorUid;
-  }
-}
-
-export interface Round {
-  $key?: string;
-  problems: CompetitionProblem[];
-  startTime: Date;
-  endTime: Date;
-}
-
-export class Round implements Round {
-  constructor(
       problems = [new CompetitionProblem()],
       startTime = new Date(),
-      endTime = new Date()) {
+      endTime = new Date(),
+      creatorUid: string = null) {
+    this.name = name;
     this.problems = problems;
     this.startTime = startTime;
     this.endTime = endTime;
+    this.creatorUid = creatorUid;
   }
 }
 
