90fa6d17c4ff29c1e38906f33c458fe02dc191dd
Tyler Berry <tyler@berryac.com>
2016-08-10 21:14:22 -0600

    list problems of competition

--- src/app/competition/select-problem/select-problem.component.ts
+++ src/app/competition/select-problem/select-problem.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ROUTER_DIRECTIVES } from '@angular/router';
+import { CompetitionProblem, RepositoryService } from '../../shared';
 
 @Component({
   moduleId: module.id,
   selector: 'app-select-problem',
   templateUrl: 'select-problem.component.html',
-  styleUrls: ['select-problem.component.css']
+  styleUrls: ['select-problem.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class SelectProblemComponent implements OnInit {
+  problems: CompetitionProblem[];
 
-  constructor() { }
+  constructor(
+      private router: Router,
+      private route: ActivatedRoute,
+      private repoService: RepositoryService) { }
 
   ngOnInit() {
+    let competitionId = this.route.snapshot.params['id'];
+    this.repoService
+        .getCompetitionProblems(competitionId)
+        .subscribe(competitionProblems => this.problems = competitionProblems);
   }
 
 }
