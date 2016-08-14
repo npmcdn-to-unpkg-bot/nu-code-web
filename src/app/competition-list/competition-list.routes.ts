e041cd6a4108d94ad3aec06829985cad52e1dcef
Tyler Berry <tyler@berryac.com>
2016-08-13 18:09:59 -0600

    seeing the list of competitions no longer requires being logged in

--- src/app/competition-list/competition-list.routes.ts
+++ src/app/competition-list/competition-list.routes.ts
@@ -1,14 +1,9 @@
 import { RouterConfig } from '@angular/router';
 import { CompetitionListComponent } from './';
-import { LoggedInGuard, VerifiedGuard } from '../shared';
 
 export const CompetitionListRoutes: RouterConfig = [
   {
     path: 'competitions',
-    component: CompetitionListComponent,
-    canActivate: [
-      LoggedInGuard,
-      VerifiedGuard
-    ]
+    component: CompetitionListComponent
   }
 ];
