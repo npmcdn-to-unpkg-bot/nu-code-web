0b23e82ee31eb3d887f25fdb6e043da465acf852
Tyler Berry <tyler@berryac.com>
2016-08-13 22:47:33 -0600

    countdown functionality and routing on scoreboard

--- src/app/competition/scoreboard/scoreboard.component.ts
+++ src/app/competition/scoreboard/scoreboard.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ROUTER_DIRECTIVES } from '@angular/router';
-import { Competition, CompetitionScoreboardRanking, RepositoryService } from '../../shared';
+import { Observable, Subscription } from 'rxjs/Rx';
+import {
+  Competition,
+  CompetitionScoreboardRanking,
+  RepositoryService,
+  TimeSpan,
+  ZeroPadPipe
+} from '../../shared';
 
 @Component({
   moduleId: module.id,
   selector: 'app-scoreboard',
   templateUrl: 'scoreboard.component.html',
   styleUrls: ['scoreboard.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES],
+  pipes: [ZeroPadPipe]
 })
 export class ScoreboardComponent implements OnInit {
   competition: Competition;
   rankings: CompetitionScoreboardRanking[];
+  remainingTime: TimeSpan;
+
+  countdown: Subscription;
 
   constructor(
       private route: ActivatedRoute,
@@ -21,7 +32,17 @@ export class ScoreboardComponent implements OnInit {
     this.route.params.subscribe(params => {
       let competitionId = params['id'];
       this.repoService.getCompetition(competitionId)
-          .subscribe(competition => this.competition = competition);
+          .subscribe(competition => {
+            this.competition = competition;
+
+            this.remainingTime = TimeSpan.until(competition.endTime);
+            if (this.countdown) {
+              this.countdown.unsubscribe();
+            }
+            this.countdown = Observable.interval(1000).subscribe(() => {
+              this.remainingTime.subtractSeconds(1);
+            });
+          });
       this.repoService.getCompetitionScoreboard(competitionId)
           .subscribe(rankings => this.rankings = rankings);
     });
