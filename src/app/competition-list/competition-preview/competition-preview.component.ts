0e03e6a3747836512de247e4494c21b57ea4d775
Tyler Berry <tyler@berryac.com>
2016-08-14 01:17:50 -0600

    acceptable colors for preview

--- src/app/competition-list/competition-preview/competition-preview.component.ts
+++ src/app/competition-list/competition-preview/competition-preview.component.ts
@@ -3,15 +3,15 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 import { Observable, Subscription } from 'rxjs/Rx';
 import * as moment from 'moment';
 import { CountdownComponent } from '../../countdown';
-import { Competition, SpacifyPipe, TimeSpan, ZeroPadPipe } from '../../shared';
+import { Competition, TimeSpan } from '../../shared';
 
 // The time in milliseconds before a competition is considered to start soon
 // 6 hours = 21600000
-const StartingSoonThreshold = 21600000;
-// The period after the end date to say "just ended"
+const startingSoonThreshold = 21600000;
+// The period after the end date to say "hard ended"
 // 10 minutes = 600000
-const JustEndedThreshold = 600000;
-const OneDayInMilliseconds = 86400000;
+const hardEndThreshold = 600000;
+const oneDayInMilliseconds = 86400000;
 
 @Component({
   moduleId: module.id,
@@ -21,8 +21,7 @@ const OneDayInMilliseconds = 86400000;
   directives: [
     ROUTER_DIRECTIVES,
     CountdownComponent
-  ],
-  pipes: [SpacifyPipe]
+  ]
 })
 export class CompetitionPreviewComponent implements OnInit, OnDestroy {
   @Input() competition: Competition;
@@ -32,29 +31,37 @@ export class CompetitionPreviewComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     let currentMilliseconds = new Date().getTime();
-    let startMilliseconds = this.competition.startTime.getTime();
-    let millisecondsBeforeStart = startMilliseconds - currentMilliseconds;
-    // Schedule more if and only if the competition starts within one day
-    if (millisecondsBeforeStart < OneDayInMilliseconds) {
-      let endMilliseconds = this.competition.endTime.getTime();
-      let startingSoonMilliseconds = millisecondsBeforeStart - StartingSoonThreshold;
-      let millisecondsBeforeJustEnded = endMilliseconds - currentMilliseconds;
-      let millisecondsBeforeHardEnded = millisecondsBeforeJustEnded + JustEndedThreshold;
+    let millisecondsUntilStart = this.competition.startTime.getTime() - currentMilliseconds;
+    let millisecondsUntilStartingSoon = millisecondsUntilStart - startingSoonThreshold;
 
-      this.scheduled = Observable.timer(startingSoonMilliseconds).subscribe(() => {
+    // Schedule more if next is within one day
+    if (millisecondsUntilStartingSoon < oneDayInMilliseconds) {
+      let millisecondsUntilEnd = this.competition.endTime.getTime() - currentMilliseconds;
+      let millisecondsUntilHardEnd = millisecondsUntilEnd + hardEndThreshold;
+
+      this.scheduled = Observable.timer(millisecondsUntilStartingSoon).subscribe(() => {
         this.state = 'StartingSoon';
 
-        this.scheduled = Observable.timer(this.competition.startTime).subscribe(() => {
-          this.state = 'Started';
+        // Schedule more if next is within one day
+        if (millisecondsUntilStart < oneDayInMilliseconds) {
+          this.scheduled = Observable.timer(this.competition.startTime).subscribe(() => {
+            this.state = 'Started';
 
-          this.scheduled = Observable.timer(millisecondsBeforeJustEnded).subscribe(() => {
-            this.state = 'JustEnded';
+            // Schedule more if next is within one day
+            if (millisecondsUntilEnd < oneDayInMilliseconds) {
+              this.scheduled = Observable.timer(millisecondsUntilEnd).subscribe(() => {
+                this.state = 'JustEnded';
 
-            this.scheduled = Observable.timer(millisecondsBeforeHardEnded).subscribe(() => {
-              this.state = 'Ended';
-            });
+                // Schedule more if next is within one day
+                if (millisecondsUntilHardEnd < oneDayInMilliseconds) {
+                  this.scheduled = Observable.timer(millisecondsUntilHardEnd).subscribe(() => {
+                    this.state = 'Ended';
+                  });
+                }
+              });
+            }
           });
-        });
+        }
       });
     }
   }
