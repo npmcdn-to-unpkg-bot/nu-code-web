8da8a66268750f57f97ca2725d7b6b0b6b6538d3
Tyler Berry <tyler@berryac.com>
2016-08-13 22:40:21 -0600

    delete now unused time and timer

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -5,7 +5,6 @@ export * from './results';
 export * from './submission';
 export * from './supported-languages';
 export * from './test-case';
-export * from './time';
 export * from './time-span';
 export * from './user';
 export * from './zero-pad';
