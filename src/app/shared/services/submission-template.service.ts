dc67f0a5c9dce161d42e4c4913a8788664907ec4
Tyler Berry <tyler@berryac.com>
2016-08-19 20:37:05 -0600

    rm java.io.* from java template

--- src/app/shared/services/submission-template.service.ts
+++ src/app/shared/services/submission-template.service.ts
@@ -53,8 +53,7 @@ func main() {
   // Read input from STDIN. Write output to STDOUT.
 }
 `,
-  'java': `import java.io.*;
-import java.util.*;
+  'java': `import java.util.*;
 
 public class Solution {
   public static void main(String[] args) {
